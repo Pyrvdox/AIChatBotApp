1cc1d21304999b6bd48f07ffe15d620e821faeac
Dawid Ciereszko <cierech8991@gmail.com>
2025-06-26 23:13:39 +0200

    template fix

--- aitemplate.py
+++ aitemplate.py
@@ -1,5 +1,3 @@
-from langchain_core.prompts import ChatMessagePromptTemplate
-
 template = """
 Answer the question below.
 There is the conversation history : {context}
--- chat.py
+++ chat.py
@@ -1,5 +1,9 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from aitemplate import template as ai_template
+
 
 model = OllamaLLM(model="llama3")
 
 model.invoke(input="")
+prompt = ChatPromptTemplate.from_template(ai_template)
